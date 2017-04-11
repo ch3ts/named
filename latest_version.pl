ebb3e7141ca59ca6ae03c7f608322e9f343840cc
Ulrich Etile <chets@cydonia.lyoko.lan>
2017-04-11 07:18:04 +0200

    optimizing loops

--- latest_version.pl
+++ latest_version.pl
@@ -5,57 +5,49 @@ use Net::FTP;
 
 
 my %connection_hash = (
-        "server"              => "ftp.isc.org",
-        "uri"                     => "/isc/bind9/",
-        "login"                => "anonymous",
+        "server"            => "ftp.isc.org",
+        "uri"                   => "/isc/bind9/",
+        "login"               => "anonymous",
         "password"      => "anonymous"
 );
+
+my @new_sorted_array = undef;
+my @versions = undef;
+
 
 my $ftp_con = Net::FTP->new( $connection_hash{"server"}, Debug => 1, Passive => 1  );
 
 $ftp_con->login( $connection_hash{ "login" }, $connection_hash{ "password" } ) or die "Cannot login ", $ftp_con->message;
 $ftp_con->cwd( $connection_hash{ "uri" } );
 
-my @dir = map( sort( $_ ) , $ftp_con->ls());
-my @versions = undef;
-
+my @dir = map( sort( $_ ) , $ftp_con->ls() );
 $ftp_con->quit;
 
 
-foreach ( @dir )
-{
-    # remove alpha, beta, rc version
-    if ( $_ !~ /rc|a|b/ ) {
-        push @versions, $_;
-    }
-}
-
-my @new_sorted_array = undef;
-my $counter = 0;
-
-foreach my $number ( reverse 0..16 )
-{
-    if ( $counter eq 0 ) {
-        foreach my $version ( @versions )
-        {
-            if ( $version =~ /9\.$number.*/ )
-            {
-                push ( @new_sorted_array, $version );
-                $counter = 1;
+map { if ( ! /rc|a|b|cur|keys/ ) {
+                push( @versions, $_ );
             }
-        }
+} @dir;
+
+my $counter = 0;
+foreach my $number ( reverse 0..16 ) {
+    if ( $counter eq 0 ) {
+        map {
+            if ( $_ =~ /9\.$number.*/ ) {
+                push ( @new_sorted_array, $_ );
+                $counter = 1;
+            }   
+        } @versions;
     }   
 }
 
-
-my $environment_variable = (reverse map( sort($_), @new_sorted_array))[0];
-
-$ENV{BIND_VERSION} = $environment_variable;
-
-open( my $fhw, ">", ".bind_version" ) or die ($!);
-print $fhw "-e BIND_VERSION=$environment_variable";
-close($fhw);
-
-
+eval {
+    open( my $fhw, ">", ".bind_version" );
+    print $fhw "-e BIND_VERSION=" . (reverse map( sort($_), @new_sorted_array))[0];
+    close($fhw);
+    1;
+} or do {
+    die( "version not found !" );
+}
 
 
